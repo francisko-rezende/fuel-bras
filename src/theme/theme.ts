576b478eb0c7690c9558a1db939fa949048626d9
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-14 18:25:43 -0300

    feat: add brand colors to app theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -4,4 +4,8 @@ export const theme = createTheme({
   typography: {
     fontFamily: "Nunito Sans, Roboto, sans-serif",
   },
+  palette: {
+    primary: { main: "#00a335" },
+    text: { primary: "#3e5055" },
+  },
 });
