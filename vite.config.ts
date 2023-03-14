41ccd41cf510c7e8cfa05ddebda22a46999cd7c8
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-14 18:26:27 -0300

    feat: add path alias leading to yup stuff

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "@lib/yup": path.resolve(__dirname, "src/lib/yup"),
       "@components": path.resolve(__dirname, "src/components"),
     },
   },
