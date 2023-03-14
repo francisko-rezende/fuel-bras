3723149869711f5e09a1c0fab4ed10d6e5d9148e
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-14 18:25:20 -0300

    feat: format ResultCard

--- src/components/ResultCard/ResultCard.tsx
+++ src/components/ResultCard/ResultCard.tsx
@@ -1,27 +1,33 @@
-
-import { Card, Stack, CardContent, Typography } from '@mui/material';
+import { Card, Stack, CardContent, Typography } from "@mui/material";
 
 interface ResultCardProps {
-  avgConsumptionPerTon: string
-  currentLicensePlate: string
+  avgConsumptionPerTon: string;
+  currentLicensePlate: string;
 }
 
-export const ResultCard = ({ avgConsumptionPerTon, currentLicensePlate }: ResultCardProps) => {
+export const ResultCard = ({
+  avgConsumptionPerTon,
+  currentLicensePlate,
+}: ResultCardProps) => {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography fontSize='1.5rem' component='h3' color="text.secondary" gutterBottom>
-          Consumo médio {Boolean(currentLicensePlate) && `• ${currentLicensePlate}`}
+        <Typography
+          fontSize="1.5rem"
+          component="h3"
+          color="text.secondary"
+          gutterBottom
+        >
+          Consumo médio{" "}
+          {Boolean(currentLicensePlate) && `• ${currentLicensePlate}`}
         </Typography>
-        <Stack direction='row' alignItems='baseline' gap='1rem'>
-          <Typography fontSize='3rem'>
-            {avgConsumptionPerTon}
-          </Typography>
-          <Typography fontSize='1.5rem' color="text.secondary">
-            litros / toneladas
+        <Stack direction="row" alignItems="baseline" gap="1rem">
+          <Typography fontSize="3rem">{avgConsumptionPerTon}</Typography>
+          <Typography fontSize="1.5rem" color="text.secondary">
+            {Boolean(avgConsumptionPerTon) && "litros / toneladas"}
           </Typography>
         </Stack>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
