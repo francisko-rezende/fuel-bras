1593f3e48170fe5fc853ff592b1b340f8e8c97e0
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-15 07:08:28 -0300

    feat: add router to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,27 @@
-import { ThemeProvider } from '@mui/material'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { theme } from './theme/theme'
+import { ThemeProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { theme } from "./theme/theme";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root, History } from "@routes";
+import { Main } from "@components";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "historico", element: <History /> },
+      { path: "/", element: <Main /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>,
-)
+);
