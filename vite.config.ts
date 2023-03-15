485ea03553ee43ccc6b92472ab59473761d459fa
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-15 19:28:19 -0300

    feat: add new path aliases for libs, configs and api fns

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,13 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "@lib": path.resolve(__dirname, "src/lib"),
       "@lib/yup": path.resolve(__dirname, "src/lib/yup"),
       "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@config": path.resolve(__dirname, "src/config"),
       "@routes": path.resolve(__dirname, "src/routes"),
       "@types": path.resolve(__dirname, "src/types"),
+      "@api": path.resolve(__dirname, "src/api"),
       "@utils": path.resolve(__dirname, "src/utils"),
       "@components": path.resolve(__dirname, "src/components"),
     },
