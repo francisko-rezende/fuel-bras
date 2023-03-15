5796e73fe9162ea5810dcab19084ac40b0263537
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-15 19:12:36 -0300

    feat: start using useHistory hook in Root

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,7 +1,10 @@
 import { Header } from "@components";
 import { Outlet } from "react-router-dom";
+import { useHistory } from "@hooks";
 
 export const Root = () => {
+  useHistory();
+
   return (
     <div
       style={{ display: "grid", gridTemplateRows: "auto 1fr", height: "100%" }}
