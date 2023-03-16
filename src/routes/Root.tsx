2928ff427f6d45ecb78533bc56ead2be73e679c8
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-16 15:52:00 -0300

    style: add some breathing room to the bottom of the page

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -4,7 +4,12 @@ import { Outlet } from "react-router-dom";
 export const Root = () => {
   return (
     <div
-      style={{ display: "grid", gridTemplateRows: "auto 1fr", height: "100%" }}
+      style={{
+        display: "grid",
+        gridTemplateRows: "auto 1fr",
+        height: "100%",
+        paddingBlockEnd: "50px",
+      }}
     >
       <Header />
       <Outlet />
